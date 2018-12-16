e44a08427c94ea1ebd33e8bee871ff498b7bc79f
reger-men <adil-lashab@hotmail.com>
2018-12-16 12:18:34 -0600

    update

--- webServer.h
+++ webServer.h
@@ -45,7 +45,7 @@ class WebServer {
       void handleLED() {                          // If a POST request is made to URI /LED
         digitalWrite(led,!digitalRead(led));      // Change the state of the LED
         server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
-        server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
+        //server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
       }
       
       // convert the file extension to the MIME type
