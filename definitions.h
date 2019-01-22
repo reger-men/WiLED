3fd2fc846e50c425050db3c79909bb35a7740fba
reger-men <adil-lashab@hotmail.com>
2019-01-22 16:36:58 -0600

    add ws2812

--- definitions.h
+++ definitions.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <stdint.h>
 
-
-#define PIN 14
-#define NUMLEDS 300
+#define SMD_STRIP   1                       // 5050 SMD RGB DC 12V 
+#define WS28_STRIP  1                       // WS2811/WS2812/WS2812b RGB DC 5V 
+#define LED_PIN     14
+#define NUM_LEDS    300
 
 enum StateMode { SET_COLOR, OFF, ON, BLINK };
 StateMode s_mode = OFF;
