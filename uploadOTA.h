26986e0faab17b8879473940652afdb520f4b366
reger-men <adil-lashab@hotmail.com>
2019-02-05 16:59:44 -0600

    wifimanger

--- uploadOTA.h
+++ uploadOTA.h
@@ -3,61 +3,63 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
 
 class UploadOTA {
   public:
 
     UploadOTA()
     {
-      WiFi.mode(WIFI_STA);
-      WiFi.begin(this->ssid_, this->password_);
-      while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("Connection Failed! Rebooting...");
+      WiFiManager wifiManager;
+      if (!wifiManager.autoConnect(ssid_, password_)) {
+        Serial.println("Failed to connect, we should reset as see if it connects");
+        delay(3000);
+        ESP.reset();
         delay(5000);
-        ESP.restart();
+      } else {
+        ArduinoOTA.onStart([]() {
+          String type;
+          if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+          } else { // U_SPIFFS
+            type = "filesystem";
+          }
+
+          // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+          Serial.println("Start updating " + type);
+        });
+
+        ArduinoOTA.onEnd([]() {
+          Serial.println("\nEnd");
+        });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+          Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+        ArduinoOTA.onError([](ota_error_t error) {
+          Serial.printf("Error[%u]: ", error);
+          if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+          } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+          } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+          } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+          } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+          }
+        });
+        ArduinoOTA.begin();
+        Serial.println("Ready");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
       }
-
-      ArduinoOTA.onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH) {
-          type = "sketch";
-        } else { // U_SPIFFS
-          type = "filesystem";
-        }
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
-      });
-
-      ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd");
-      });
-      ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-      });
-      ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) {
-          Serial.println("Auth Failed");
-        } else if (error == OTA_BEGIN_ERROR) {
-          Serial.println("Begin Failed");
-        } else if (error == OTA_CONNECT_ERROR) {
-          Serial.println("Connect Failed");
-        } else if (error == OTA_RECEIVE_ERROR) {
-          Serial.println("Receive Failed");
-        } else if (error == OTA_END_ERROR) {
-          Serial.println("End Failed");
-        }
-      });
-      ArduinoOTA.begin();
-      Serial.println("Ready");
-      Serial.print("IP address: ");
-      Serial.println(WiFi.localIP());
     }
 
   protected:
-    const char* ssid_ = "hama";
-    const char* password_ = "Andrea0111";
+    char const* ssid_ = "WiLED";
+    char const* password_ = "WiLED";
 
   private:
 
